d799ee83e38daa6d588842eb2551a192930d70b2
stew3254 <rtstewy@gmail.com>
2020-11-07 23:16:11 -0500

    Add ability for directories to work as well

--- src/util.go
+++ src/util.go
@@ -20,24 +20,43 @@ type htmlStrippingFileSystem struct {
 	http.FileSystem
 }
 
+// UNAVOIDABLE BUG: When going to a directory index.html,
+// the url will not have a trailing slash
 // Open is a wrapper around the Open method of the embedded FileSystem
 // This allows us to take urls without a file extension and open them as a .html
 func (fs htmlStrippingFileSystem) Open(name string) (http.File, error) {
-	path := name
 
-	if len(strings.Split(path, ".")) == 1 {
-		if path == "/" {
+	// Directory doesn't exist, now checking 
+	if len(strings.Split(name, ".")) == 1 {
+		if name == "/" {
 			//Do nothing
-		} else if strings.HasSuffix(path, "/"){
-			path += "index.html"
+		} else if strings.HasSuffix(name, "/"){
+			name += "index.html"
 		} else {
-			path += ".html"
+			// See if a file exists as a directory already
+			file, err := fs.FileSystem.Open(name)
+			if err == nil {
+				fi, err := file.Stat()
+				if err != nil {
+					return file, err
+				}
+
+				// Prioritize directories over html files with the same name
+				if fi.IsDir() {
+					name += "/index.html"
+				} else {
+					return file, err
+				}
+			} else {
+				// Not a directory, so add the html
+				name += ".html"
+			}
 		}
 	}
 
-	fmt.Println(path)
+	fmt.Println(name)
 
-	file, err := fs.FileSystem.Open(path)
+	file, err := fs.FileSystem.Open(name)
 	if err != nil {
 		return nil, err
 	}
